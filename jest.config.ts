7645685ecd2128c70ba0e6c42c77c0288861f9a8
Benaor <benjamingirard25@gmail.com>
2024-05-15 21:29:06 +0200

    feat(config): create configuration

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import { pathsToModuleNameMapper } from "ts-jest";
+
+import { compilerOptions } from "./tsconfig.json";
+import type { JestConfigWithTsJest } from "ts-jest";
+
+const jestConfig: JestConfigWithTsJest = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    testMatch: ["<rootDir>/tests/**/*.test.ts"],
+    moduleFileExtensions: ["ts", "js"],
+    roots: ["<rootDir>"],
+    modulePaths: [compilerOptions.baseUrl],
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>/" })
+};
+
+export default jestConfig;
